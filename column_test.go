be09e5679a1de9591afd6c3e5c8aaa522645609b
Jim Lambert <jlambert@hashicorp.com>
2020-06-02 07:53:15 -0400

    refactor tests

--- column_test.go
+++ column_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func Test_Column(t *testing.T) {
+func TestDbAsserts_Column(t *testing.T) {
 	t.Parallel()
 	cleanup, conn, _ := TestSetup(t, "postgres")
 	defer func() {
@@ -15,10 +15,14 @@ func Test_Column(t *testing.T) {
 			t.Error(err)
 		}
 	}()
-	t.Run("columns", func(t *testing.T) {
-		assert := New(t, conn, "postgres")
-		assert.Column(
-			ColumnInfo{
+	cases := []struct {
+		name   string
+		column ColumnInfo
+		want   bool
+	}{
+		{
+			name: "nullable",
+			column: ColumnInfo{
 				TableName:  "test_table_dbasserts",
 				Name:       "public_id",
 				Default:    "",
@@ -26,10 +30,75 @@ func Test_Column(t *testing.T) {
 				DomainName: "dbasserts_public_id",
 				IsNullable: false,
 			},
-		)
-	})
+			want: true,
+		},
+		{
+			name: "bad type",
+			column: ColumnInfo{
+				TableName:  "test_table_dbasserts",
+				Name:       "public_id",
+				Default:    "",
+				Type:       "bad type",
+				DomainName: "dbasserts_public_id",
+				IsNullable: false,
+			},
+			want: false,
+		},
+		{
+			name: "bad column name",
+			column: ColumnInfo{
+				TableName:  "test_table_dbasserts",
+				Name:       "bad_column_name",
+				Default:    "",
+				Type:       "text",
+				DomainName: "dbasserts_public_id",
+				IsNullable: false,
+			},
+			want: false,
+		},
+		{
+			name: "bad table name",
+			column: ColumnInfo{
+				TableName:  "bad_name",
+				Name:       "public_id",
+				Default:    "",
+				Type:       "text",
+				DomainName: "dbasserts_public_id",
+				IsNullable: false,
+			},
+			want: false,
+		},
+		{
+			name: "bad domain",
+			column: ColumnInfo{
+				TableName:  "test_table_dbasserts",
+				Name:       "public_id",
+				Default:    "",
+				Type:       "text",
+				DomainName: "bad_domain",
+				IsNullable: false,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			mockery := new(MockTesting)
+			a := New(mockery, conn, "postgres")
+
+			if got := a.Column(tt.column); got != tt.want {
+				t.Errorf("DbAsserts.Nullable() = %v, want %v", got, tt.want)
+			}
+			switch {
+			case tt.want:
+				mockery.AssertNoError(t)
+			default:
+				mockery.AssertError(t)
+			}
+		})
+	}
 }
-func Test_ColumnNullable(t *testing.T) {
+func TestDbAsserts_Domain(t *testing.T) {
 	t.Parallel()
 	cleanup, conn, _ := TestSetup(t, "postgres")
 	defer func() {
@@ -40,21 +109,61 @@ func Test_ColumnNullable(t *testing.T) {
 			t.Error(err)
 		}
 	}()
-	t.Run("nullable", func(t *testing.T) {
-		mockery := new(MockTesting)
-		assert := New(mockery, conn, "postgres")
-		assert.Nullable("test_table_dbasserts", "nullable")
-		mockery.AssertNoError(t)
-	})
-	t.Run("typeInt", func(t *testing.T) {
-		mockery := new(MockTesting)
-		assert := New(mockery, conn, "postgres")
-		assert.Nullable("test_table_dbasserts", "typeInt")
-		mockery.AssertError(t)
-	})
+	cases := []struct {
+		name      string
+		tableName string
+		colName   string
+		domain    string
+		want      bool
+	}{
+		{
+			name:      "nullable",
+			tableName: "test_table_dbasserts",
+			colName:   "nullable",
+			domain:    "dbasserts_public_id",
+			want:      false,
+		},
+		{
+			name:      "public_id",
+			tableName: "test_table_dbasserts",
+			colName:   "public_id",
+			domain:    "dbasserts_public_id",
+			want:      true,
+		},
+		{
+			name:      "bad_column",
+			tableName: "test_table_dbasserts",
+			colName:   "bad_column",
+			domain:    "dbasserts_public_id",
+			want:      false,
+		},
+		{
+			name:      "bad_table",
+			tableName: "bad_table",
+			colName:   "public_id",
+			domain:    "dbasserts_public_id",
+			want:      false,
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			mockery := new(MockTesting)
+			a := New(mockery, conn, "postgres")
+
+			if got := a.Domain(tt.tableName, tt.colName, tt.domain); got != tt.want {
+				t.Errorf("DbAsserts.Nullable() = %v, want %v", got, tt.want)
+			}
+			switch {
+			case tt.want:
+				mockery.AssertNoError(t)
+			default:
+				mockery.AssertError(t)
+			}
+		})
+	}
 }
 
-func Test_ColumnDomain(t *testing.T) {
+func TestDbAsserts_Nullable(t *testing.T) {
 	t.Parallel()
 	cleanup, conn, _ := TestSetup(t, "postgres")
 	defer func() {
@@ -65,16 +174,51 @@ func Test_ColumnDomain(t *testing.T) {
 			t.Error(err)
 		}
 	}()
-	t.Run("public_id", func(t *testing.T) {
-		mockery := new(MockTesting)
-		assert := New(mockery, conn, "postgres")
-		assert.Domain("test_table_dbasserts", "public_id", "dbasserts_public_id")
-		mockery.AssertNoError(t)
-	})
-	t.Run("nullable", func(t *testing.T) {
-		mockery := new(MockTesting)
-		assert := New(mockery, conn, "postgres")
-		assert.Domain("test_table_dbasserts", "nullable", "dbasserts_public_id")
-		mockery.AssertError(t)
-	})
+	cases := []struct {
+		name      string
+		tableName string
+		colName   string
+		want      bool
+	}{
+		{
+			name:      "nullable",
+			tableName: "test_table_dbasserts",
+			colName:   "nullable",
+			want:      true,
+		},
+		{
+			name:      "typeInt-bad-colname",
+			tableName: "test_table_dbasserts",
+			colName:   "typeInt",
+			want:      false,
+		},
+		{
+			name:      "type_int",
+			tableName: "test_table_dbasserts",
+			colName:   "type_int",
+			want:      true,
+		},
+		{
+			name:      "public_id",
+			tableName: "test_table_dbasserts",
+			colName:   "public_id",
+			want:      false,
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			mockery := new(MockTesting)
+			a := New(mockery, conn, "postgres")
+
+			if got := a.Nullable(tt.tableName, tt.colName); got != tt.want {
+				t.Errorf("DbAsserts.Nullable() = %v, want %v", got, tt.want)
+			}
+			switch {
+			case tt.want:
+				mockery.AssertNoError(t)
+			default:
+				mockery.AssertError(t)
+			}
+		})
+	}
 }
