e3e5223b58d66c86d049a23b6713df7e029aa505
Michael Gaffney <mike@gaffney.cc>
2020-06-02 09:41:10 -0400

    Fix package level doc

--- doc.go
+++ doc.go
@@ -3,20 +3,19 @@
 //
 // Example Usage:
 //
-// import (
-// 	"testing"
+//	import (
+//		"testing"
 //
-// 	"github.com/hashicorp/dbassert"
-// )
+//		"github.com/hashicorp/dbassert"
+//	)
 //
-// func TestSomeDatabase(t *testing.T) {
-//	db, err := sql.Open("postgres", "postgres://postgres:secret@localhost:%s?sslmode=disable")
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	defer db.Close()
-// 	dbassert := dbassert.New(t, conn, "postgres")
-// 	dbassert.Nullable("some_table_name", "some_column")
-// }
-
+//	func TestSomeDatabase(t *testing.T) {
+//		db, err := sql.Open("postgres", "postgres://postgres:secret@localhost:%s?sslmode=disable")
+//		if err != nil {
+//			t.Fatal(err)
+//		}
+//		defer db.Close()
+//		dbassert := dbassert.New(t, conn, "postgres")
+//		dbassert.Nullable("some_table_name", "some_column")
+//	}
 package dbassert
