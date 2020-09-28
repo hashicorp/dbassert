d30d67695f973ca83ebd7b138d7255b09c3db0ae
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2020-09-28 15:30:22 -0400

    Separate out docker support into known working platforms

--- testing.go
+++ testing.go
@@ -2,12 +2,17 @@ package dbassert
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
-	"strings"
 	"testing"
 
 	_ "github.com/lib/pq"
-	"github.com/ory/dockertest/v3"
+)
+
+var (
+	StartDbInDocker = startDbInDockerUnsupported
+
+	ErrDockerUnsupported = errors.New("docker is not currently supported on this platform")
 )
 
 // MockTesting provides a testing.T mock.
@@ -77,50 +82,6 @@ func TestSetup(t *testing.T, dialect string) (func() error, *sql.DB, string) {
 	return cleanup, db, url
 }
 
-// StartDbInDocker starts up the dialect db in the local docker.
-func StartDbInDocker(dialect string) (cleanup func() error, retURL, container string, err error) {
-	pool, err := dockertest.NewPool("")
-	if err != nil {
-		return func() error { return nil }, "", "", fmt.Errorf("could not connect to docker: %w", err)
-	}
-
-	var resource *dockertest.Resource
-	var url string
-	switch dialect {
-	case "postgres":
-		resource, err = pool.Run("postgres", "latest", []string{"POSTGRES_PASSWORD=secret", "POSTGRES_DB=watchtower"})
-		url = "postgres://postgres:secret@localhost:%s?sslmode=disable"
-	default:
-		panic(fmt.Sprintf("unknown dialect %q", dialect))
-	}
-	if err != nil {
-		return func() error { return nil }, "", "", fmt.Errorf("could not start resource: %w", err)
-	}
-
-	cleanup = func() error {
-		return cleanupDockerResource(pool, resource)
-	}
-
-	url = fmt.Sprintf(url, resource.GetPort("5432/tcp"))
-
-	if err := pool.Retry(func() error {
-		db, err := sql.Open(dialect, url)
-		if err != nil {
-			return fmt.Errorf("error opening %s dev container: %w", dialect, err)
-		}
-
-		if err := db.Ping(); err != nil {
-			return err
-		}
-		defer db.Close()
-		return nil
-	}); err != nil {
-		return func() error { return nil }, "", "", fmt.Errorf("could not connect to docker: %w", err)
-	}
-
-	return cleanup, url, resource.Container.Name, nil
-}
-
 func initStore(t *testing.T, db *sql.DB) error {
 	const (
 		createDomainType = `
@@ -151,17 +112,6 @@ comment on table test_table_dbasserts is
 	return nil
 }
 
-// cleanupDockerResource will clean up the dockertest resources
-func cleanupDockerResource(pool *dockertest.Pool, resource *dockertest.Resource) error {
-	var err error
-	for i := 0; i < 10; i++ {
-		err = pool.Purge(resource)
-		if err == nil {
-			return nil
-		}
-	}
-	if strings.Contains(err.Error(), "No such container") {
-		return nil
-	}
-	return fmt.Errorf("Failed to cleanup local container: %s", err)
+func startDbInDockerUnsupported(string) (func() error, string, string, error) {
+	return nil, "", "", ErrDockerUnsupported
 }
